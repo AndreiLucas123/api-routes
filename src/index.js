80ccafab4ad10b8379947ce57d9a91b38eea539a
Andrei Lucas Gonçalves <andrei.lucas.goncalves@hotmail.com>
2020-04-03 09:33:24 -0300

    Renamed to api-routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 #!/usr/bin/env node
-
-
-if (process.argv[2]) {
-    require('./new-api-file')(process.argv[2])
-    require('./task-resolve-api-files')()
-} else {
-    require('./task-resolve-api-files')()
-}
+require('./resolve-routes')()
